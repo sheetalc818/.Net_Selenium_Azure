c76225155c127cfc687b29c0329985bfc3491c7d
Abhishek Bodake <rishibodake@gmail.com>
2020-07-10 12:50:54 +0530

    WelCome To Test

---
+++ TestCI/Google.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace TestCI
+{
+    [TestClass]
+    public class Google
+    {
+        [TestMethod]
+        public void SearchOption()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
+            {
+                //Notice navigation is slightly different than the Java version
+                //This is because 'get' is a keyword in C#
+                driver.Navigate().GoToUrl("http://www.google.com/");
+
+                // Find the text input element by its name
+                IWebElement query = driver.FindElement(By.Name("q"));
+
+                // Enter something to search for
+                query.SendKeys("Cheese");
+
+                // Now submit the form. WebDriver will find the form for us from the element
+                query.Submit();
+
+                // Google's search is rendered dynamically with JavaScript.
+                // Wait for the page to load, timeout after 10 seconds
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+
+                // Should see: "Cheese - Google Search" (for an English locale)
+                Assert.AreEqual(driver.Title, "Cheese - Google Search");
+            }
+        }
+    }
+}
